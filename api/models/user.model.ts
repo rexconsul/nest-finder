7ca4d84551699c9a30c773dd71e6f427842ad21d
RConsul <reginrexconsul@gmail.com>
2024-09-13 17:43:05 +0800

    create user model

---
+++ api/models/user.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema: Schema = new mongoose.Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
